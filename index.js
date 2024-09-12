6b5aa1f02d7077b865bbd5db496e694ad39f13f5
Nicole Riera <150852807+NicoRi5@users.noreply.github.com>
2024-09-12 11:08:37 -0400

    Fix merge issue 2

---
+++ index.js
@@ -0,0 +1,28 @@
+// Write a pseudocode (steps + comment it out)
+// Create a string using console.log
+// Assign three variables using var name = ; and include a calculation using a arithmetic operator to equal one of the three codes in the combination
+// Add comments throughout the code to explain each steps
+// create a dialog box displaying the codes and text
+
+/* string to explain the output to readers
+ */
+console.log(
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination: "
+);
+/*
+ assigning variables using unique equations and arithmetic operators
+ */
+var firstNum = 20 / 2;
+var secondNum = 99 - 59;
+var thirdNum = 23 + 16;
+
+/*
+ strings for letting the reader know what will be printed
+ followed by the vault code
+ */
+let str1 = "Vault Code: ";
+
+/*
+alert (or dialog box) to display string and code
+ */
+alert(str1 + firstNum + " " + secondNum + " " + thirdNum);
